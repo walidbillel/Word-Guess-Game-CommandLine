62641648a5401f75325c7005018566566f9d8df4
willou <walid.boubguira@gmail.com>
2018-07-25 01:28:42 -0400

    almost finished

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ var wordUsedForGame;
 
 
 
-// function to start the game
+
+// Function To Start The Game
 function startGame() {
     // Randomly selecting a word from our array
     wordUsedForGame = hangManWordChoices[Math.floor(Math.random() * hangManWordChoices.length)];
@@ -20,19 +21,25 @@ function startGame() {
     // calling the renderWord function to push the letters into the word
     wordConstructed.renderWord();
 
-    console.log(".....................");
+    console.log("\n");
     console.log("Guess The Word. You have 7 chances to get it Right!!!");
     console.log(" Hint: The Words are computer Languages \n");
 
+    // Displaying the dashes based on the word selected using the Word display function 
+    // they will be dashes because the guessed is set to false
     console.log(wordConstructed.display());
+    console.log("\n");
+
+    // calling askLetters to run the game
     askLetters();
-    // console.log(wordConstructed);
+
+
 }
 
 
 
+// Function To Ask The Letter from The user using inquirer.prompt
 function askLetters() {
-
 
 
     inquirer.prompt([
@@ -43,26 +50,40 @@ function askLetters() {
         }
     ]).then(function (UserData) {
 
+
+        // Passing in the user input as a parameter for the Word updateLetter Function to check if it's wrong or right
         wordConstructed.updateLetter(UserData.userInputGuess);
-    
+
+        // running display to switch whatever was guessed each time
         console.log(wordConstructed.display());
 
+        // Guessed Wrong when the array length is not empty
         if (wordConstructed.wrongLetters.length > 0) {
+
+            // logging the array of wrong letters
             console.log("wrong letters: " + wordConstructed.wrongLetters);
             console.log(" ");
+            // logging the remainning guesses
+            console.log("Remaining Guesses:  " + (7 - wordConstructed.wrongLetters.length));
         }
 
+        // If the wrong letters array reaches 7 that means the 7 guesses are finish so the game is over
+        if (wordConstructed.wrongLetters.length === 7) {
 
-        if (wordConstructed.wrongLetters.length == 7) {
             console.log("You lost - the correct word was " + wordUsedForGame + ".");
-        } else {
-            if (wordConstructed.display().indexOf("_") != -1) {
-                askLetters();
-            } else {
-                console.log("Great Job!!!! You Rock");
-            }
+
+
+        // If the wrong letters array reaches is less than 7 and the dashes in the word are still there we keep asking the letters
+        } else if (wordConstructed.wrongLetters.length < 7 && wordConstructed.display().indexOf("_") != -1) {
+            askLetters();
+            // If the dashes are finished in the word that means the user guessed all letters So Winnn!!
+        } else if (wordConstructed.display().indexOf("_") === -1){
+            console.log("Great Job!!!! You Rock");
         }
     });
+
+
 }
 
+// Call our startGame function to start playing
 startGame();
--- letter.js
+++ letter.js
@@ -1,8 +1,12 @@
+// Creating a Letter constructor 
 function Letter(character) {
 
+    // the paramter passed (character) 
     this.character = character;
+    // We set guessed to false so we can change it later when it's guessed
     this.guessed = false;
 
+    // Function that will display the character or _ based on the guessed
     this.display = function () {
         if (this.guessed) {
             return " " + this.character;
@@ -13,12 +17,6 @@ function Letter(character) {
 
 }
 
-// var lettero = new Letter("s");
-
-// console.log(lettero.display());
-
-
-
-
+// Importing the Letter so we can use it in building the Word constructor
 module.exports = Letter;
 
